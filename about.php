89bcb5d9af6686878321c2809ad4c07036c0ae2a
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-07-07 20:05:41 -0300

    About

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php /* Template Name: about */ ?>
+
+<?php get_template_part( 'templates/head' ) ?>
+
+<?php $header_title = 'Sobre Nós' ?>
+<?php $header_subtitle = 'Não há ninguém que ame a dor por si só, que a busque e queira tê-la, simplesmente por ser dor...' ?>
+<?php include( locate_template( 'templates/header-title.php' ) ) ?>
+
+<div class="container text-justify">
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit quam et diam consectetur, vitae ullamcorper est cursus. Mauris auctor mauris a rhoncus fermentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse risus tortor, aliquam sit amet diam sed, condimentum consectetur ligula. Sed purus mauris, facilisis sed enim et, hendrerit sagittis enim. Proin vel gravida nunc. In tincidunt nibh nisi, eget condimentum elit posuere ac. Nullam sollicitudin
+        diam
+        eget magna ultricies, vel dignissim odio lacinia. Suspendisse potenti. Donec non nulla ligula. Cras nec euismod leo, ac elementum dui. Duis gravida leo magna, quis convallis sem varius nec. Duis nec viverra dui. Aliquam interdum urna tellus, in commodo neque finibus venenatis.</p>
+
+    <p>Proin vulputate rutrum risus. Duis pellentesque ante id dolor gravida, vitae convallis odio pulvinar. Quisque eu tellus vel risus dictum rhoncus ac et dui. Donec id varius quam, in imperdiet nisi. Ut fringilla mauris erat, mattis pulvinar ipsum dictum sed. Fusce laoreet faucibus ligula vitae hendrerit. Quisque at rhoncus arcu. Nullam a vehicula mi. Aliquam venenatis malesuada magna varius iaculis. Nam finibus sollicitudin odio sed pellentesque. Pellentesque quis efficitur purus. Duis
+        quis
+        elementum sapien, sed bibendum ipsum. Mauris eget mauris in ante iaculis commodo. Suspendisse congue, risus ac fermentum commodo, elit lorem auctor augue, eget mattis elit sapien quis lectus. Fusce viverra mauris sit amet aliquet sagittis. Quisque placerat tellus ante, vitae viverra sem volutpat eget.</p>
+
+    <p>Proin mattis, nunc sit amet finibus pellentesque, eros eros facilisis dui, et pulvinar urna justo quis turpis. Vestibulum pretium, velit ac posuere blandit, tortor arcu bibendum turpis, id ultrices diam dui in nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum, nulla at malesuada molestie, ante massa hendrerit libero, et condimentum ligula dolor quis velit. Pellentesque et tempor nulla, et fermentum lectus. Suspendisse consequat, odio ut fringilla tincidunt,
+        massa orci sodales sem, vel cursus est dolor eget metus. Sed ullamcorper dictum quam, ut vehicula est mattis in. Nam dignissim nisl vel est molestie, vel eleifend dolor ultricies. Mauris quis dolor eros. Aliquam volutpat rhoncus dui vehicula faucibus. Phasellus finibus est bibendum felis dignissim mattis. Praesent finibus suscipit risus, sed iaculis turpis. Sed ullamcorper sit amet metus ut mollis. Aenean blandit nibh sed leo tincidunt tincidunt. Suspendisse auctor elit velit, ac
+        venenatis nisi accumsan eget.</p>
+
+    <p>Donec consectetur turpis et tellus porttitor, a consectetur mi interdum. Aliquam vitae diam a tortor varius convallis. Maecenas bibendum condimentum justo sed auctor. Suspendisse sed posuere lectus. Praesent a feugiat magna. Fusce ac vestibulum sem, a tincidunt eros. Integer mollis tellus sed sodales eleifend. Morbi molestie erat vitae leo tincidunt dapibus. Pellentesque vehicula condimentum ultrices. In nunc magna, condimentum ut tincidunt non, posuere nec orci. Pellentesque eu
+        mattis
+        odio. Nullam vel ante augue. Praesent eu ligula faucibus, pharetra sem ut, vulputate magna. Aliquam sit amet ex posuere est elementum rutrum nec non mi. Praesent vehicula mollis rhoncus.</p>
+
+    <p>In elementum condimentum rhoncus. Fusce est ex, congue a velit id, venenatis posuere sem. Suspendisse vestibulum maximus enim sit amet faucibus. Maecenas tortor ante, posuere auctor lacinia eu, ultricies id lorem. Phasellus at purus id diam dapibus gravida vitae sit amet risus. Nullam sed malesuada enim. Donec leo ipsum, malesuada eget purus tristique, pellentesque porttitor velit. Vivamus ut est dolor. Nunc viverra, metus in luctus sollicitudin, lectus purus commodo diam, vitae
+        efficitur
+        augue turpis at arcu. Aenean cursus vehicula imperdiet. Curabitur blandit egestas felis, ornare porttitor nibh vulputate ac. Vivamus metus risus, elementum quis nisl at, ultricies rhoncus ante. Praesent id pellentesque purus. Quisque a neque ac ipsum vehicula convallis eget sit amet nisi. Ut urna justo, tempus vel justo a, fermentum sodales dui.</p>
+</div>
+
+<?php get_template_part( 'templates/footer' ) ?>
--- templates/header-title.php
+++ templates/header-title.php
@@ -1,5 +1,8 @@
 <div class="jumbotron post-title bg-dark post-title">
     <div class="container">
         <h1 class="display-5 text-uppercase text-center text-light"><?= $header_title ?></h1>
+		<?php if ( isset( $header_subtitle ) ): ?>
+            <h3 class="display-1h mt-5 text-center text-light"><em><?= $header_subtitle ?></em></h3>
+		<?php endif; ?>
     </div>
 </div>
