25a2753d7686f3911a8ed8708c987137e54a201f
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-07-07 21:31:11 -0300

    logo

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top justify-content-between">
     <div class="container">
-        <a class="navbar-brand" href="<?php bloginfo( 'url' ) ?>"><?php bloginfo( 'name' ) ?></a>
+        <a href="/">
+            <img src="<?php bloginfo( 'template_url' ); ?>/assets/dist/images/logo.png" class="img-fluid" style="max-height: 40px">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-between" id="main-navbar">
-			<?php robok_main_nav_left() ?>
-			<?php robok_main_nav_right() ?>
+            <div><?php robok_main_nav_left() ?></div>
+            <div><?php robok_main_nav_right() ?></div>
         </div>
     </div>
 </nav>
