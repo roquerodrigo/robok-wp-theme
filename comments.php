8590a84b5631a2cce7c5a34dca4a7b791c3d4506
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-07-25 00:00:51 -0300

    comments

---
+++ comments.php
@@ -0,0 +1,2 @@
+<h4 class="text-uppercase mt-4">Deixe seu comentário</h4>
+<div class="fb-comments" data-numposts="5" data-width="100%"></div>
--- templates/head.php
+++ templates/head.php
@@ -21,6 +21,15 @@
         ga('create', 'UA-99250107-1', 'auto');
         ga('send', 'pageview');
     </script>
+    <div id="fb-root"></div>
+    <script>(function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10&appId=479585005752772";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
     <meta name="theme-color" content="#212121">
 </head>
 <body>
