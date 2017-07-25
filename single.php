3ef3c341afacba5adf236c9ba457f1c03a241ea1
Rodrigo Roque <rodrigoroque@unifei.edu.br>
2017-07-25 00:00:55 -0300

    .

--- single.php
+++ single.php
@@ -7,12 +7,14 @@
 
     <div class="container">
 
-        <div class="row mb-4">
-            <div class="col">
-                <img src="<?php the_post_thumbnail_url() ?>" class="img-fluid w-100">
-            </div>
-        </div>
+        <p>
+            <i class="mdi mdi-clock"></i>
+		    <?php the_time( 'j \d\e F \d\e Y H:i' ); ?>
 
+            <i class="mdi mdi-account ml-2"></i>
+		    <?php the_author() ?>
+        </p>
+        <hr>
 		<?= apply_filters( 'the_content', get_the_content() ) ?>
 
 		<?php if ( function_exists( 'dw_reactions' ) ) {
